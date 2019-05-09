db95c826ee9ec05cd2596053db55f01618d78c78
wayshon <wayshon@qq.com>
2019-05-09 21:57:26 +0800

    nav add idea project

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,7 +9,15 @@ module.exports = {
 			{ text: '主页', link: '/' },
 			{ text: '归档', link: '/archives' },
 			{ text: '关于我', link: '/about' },
-			{ text: 'Github', link: 'https://www.github.com/wayshon' }
+			{ text: 'Github', link: 'https://www.github.com/wayshon' },
+			{ text: 'idea project',
+              items: [
+                { text: '关心宝', link: 'https://itunes.apple.com/cn/app/id1447258000' },
+                { text: 'qr code', link: 'https://imgqr.cn/' },
+                { text: 'base64', link: 'http://base64.imgqr.cn/' },
+                { text: 'nes', link: 'https://calcbit.com/nes/' }
+              ] 
+            }
 		],
         activeHeaderLinks: false,
 	},
